ebcd38ac885496b971b5bd718f77ec171f942076
Niklas Bakula <niklasjbakula@gmail.com>
2023-02-24 14:11:16 +0000

    Finished routing, controllers and actions

---
+++ app/models/bookmark.rb
@@ -0,0 +1,11 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
+
+  validates_associated :movie
+  validates_associated :list
+
+end
